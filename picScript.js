a401741a8f1d2db86d59c6711fca4069677da187
David Tam <D@Davids-MacBook-Pro.local>
2015-11-14 23:28:37 -0600

    Submission for Assignment 2

---
+++ picScript.js
@@ -0,0 +1,101 @@
+// JavaScript Document
+
+$(document).ready(function(){
+	    $("#div1").fadeIn();
+        $("#div2").fadeOut();
+        $("#div3").fadeOut();
+		$("#div4").fadeOut();
+		$("#div5").fadeOut();
+        $("#div6").fadeOut();
+		$("#div7").fadeOut();
+		$("#div8").fadeOut();
+		
+	$("#p1").click(function(){
+        $("#div1").fadeIn();
+        $("#div2").fadeOut();
+        $("#div3").fadeOut();
+		$("#div4").fadeOut();
+		$("#div5").fadeOut();
+        $("#div6").fadeOut();
+		$("#div7").fadeOut();
+		$("#div8").fadeOut();
+	});
+		
+	$("#p2").click(function(){
+        $("#div1").fadeOut();
+        $("#div2").fadeIn();
+        $("#div3").fadeOut();
+		$("#div4").fadeOut();
+		$("#div5").fadeOut();
+        $("#div6").fadeOut();
+		$("#div7").fadeOut();
+		$("#div8").fadeOut();
+	});
+	
+	$("#p3").click(function(){
+        $("#div1").fadeOut();
+        $("#div2").fadeOut();
+        $("#div3").fadeIn();
+		$("#div4").fadeOut();
+		$("#div5").fadeOut();
+        $("#div6").fadeOut();
+		$("#div7").fadeOut();
+		$("#div8").fadeOut();
+	});
+	
+	$("#p4").click(function(){
+        $("#div1").fadeOut();
+        $("#div2").fadeOut();
+        $("#div3").fadeOut();
+		$("#div4").fadeIn();
+		$("#div5").fadeOut();
+		$("#div6").fadeOut();
+        $("#div7").fadeOut();
+		$("#div8").fadeOut();
+    });
+	
+	$("#p5").click(function(){
+        $("#div1").fadeOut();
+        $("#div2").fadeOut();
+        $("#div3").fadeOut();
+		$("#div4").fadeOut();
+		$("#div5").fadeIn();
+        $("#div6").fadeOut();
+		$("#div7").fadeOut();
+		$("#div8").fadeOut();
+    });
+	
+	$("#p6").click(function(){
+        $("#div1").fadeOut();
+        $("#div2").fadeOut();
+		$("#div3").fadeOut();
+		$("#div4").fadeOut();
+        $("#div5").fadeOut();
+        $("#div6").fadeIn();
+		$("#div7").fadeOut();
+		$("#div8").fadeOut();
+    });
+	
+	$("#p7").click(function(){
+        $("#div1").fadeOut();
+        $("#div2").fadeOut();
+		$("#div3").fadeOut();
+		$("#div4").fadeOut();
+        $("#div5").fadeOut();
+        $("#div6").fadeOut();
+		$("#div7").fadeIn();
+		$("#div8").fadeOut();
+    });
+	
+	$("#p8").click(function(){
+        $("#div1").fadeOut();
+        $("#div2").fadeOut();
+		$("#div3").fadeOut();
+		$("#div4").fadeOut();
+        $("#div5").fadeOut();
+        $("#div6").fadeOut();
+		$("#div7").fadeOut();
+		$("#div8").fadeIn();
+    });
+	
+});
